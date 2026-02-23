198656186070964b18146139b779729cb3328d05
brentjan <brentjan.janssens@ugent.be>
2026-02-23 12:41:59 +0100

    feat: implemented simple q network

--- src/brittle_star_locomotion/q_network/QNetwork.py
+++
@@ -1,5 +0,0 @@
-import flax
-
-
-if __name__ == "__main__":
-    print("Flax version:", flax.__version__)
---
+++ src/brittle_star_locomotion/q_network/q_network.py
@@ -0,0 +1,23 @@
+from flax import nnx
+import jax
+import jax.numpy as jnp
+
+
+class QNetwork(nnx.Module):
+    def __init__(self, input_size: int, output_size: int, rngs: nnx.Rngs, hidden_size: int = 64):
+        self.mlp = [
+            nnx.Linear(input_size, hidden_size, rngs=rngs),
+            nnx.relu,
+            nnx.Linear(hidden_size, output_size, rngs=rngs)
+        ]
+
+
+    def __call__(self, x: jax.Array) -> jax.Array:
+        for layer in self.mlp:
+            x = layer(x)
+        return x
+
+
+if __name__ == "__main__":
+    import flax
+    print("Flax version:", flax.__version__)
