68176023ebecef646e6c4d578f86ba5a2b597b57
brentjan <brentjan.janssens@ugent.be>
2026-04-06 20:38:55 +0200

    chore: trying to minimize optimize function

--- src/brittle_star_locomotion/optimizer/iql_optimizer.py
+++ src/brittle_star_locomotion/optimizer/iql_optimizer.py
@@ -1,5 +1,6 @@
 import time
 
+from brittle_star_locomotion.config.configuration import Configuration
 from brittle_star_locomotion.environment.environment import Environment
 from brittle_star_locomotion.neural.qnetwork import QNetwork
 
@@ -12,30 +13,26 @@ import jax.numpy as jnp
 
 class IQLOptimizer:
     def __init__(self, environment: Environment):
+        self._config = Configuration().configuration
+        self._seed = self._config.rl.seed # TODO: config
+        self._rng = jax.random.PRNGKey(self._seed)
+
+        self._epsilon = self._config.rl.epsilon # TODO: config
         self._environment = environment
-
-        self._config = None # TODO: load config file
-
         self._observation_size = 16 # TODO: actual observation size
 
         self._replay_buffers = [
             self._create_replay_buffer() 
             for _ in range(self._config.rl.num_agents) # TODO: config
         ]
-
-        self._seed = 42 # TODO: load seed from config file
-        self._rng = jax.random.PRNGKey(self._seed)
-
         self._q_networks = [
             self._create_qnetwork(agent_id) 
             for agent_id in range(self._config.rl.num_agents) # TODO: config
         ]
-
         self._target_q_networks = [
             self._create_qnetwork(agent_id)
             for agent_id in range(self._config.rl.num_agents) # TODO: config
         ]
-
         self._synchronize_target_networks()
 
         optimizer = optax.chain(
@@ -46,77 +43,42 @@ class IQLOptimizer:
             for q_network in self._q_networks
         ]
 
-        # Hyperparameters
-        self._epsilon = self._config.rl.epsilon # TODO config
-
     def optimize(self):
-        n_environments = self._config.env.n_environments # TODO config
-        batch_size = self._config.rl.batch_size # TODO config
+        n_environments = self._config.env.n_environments # TODO: config
+        total_train_steps = 0
 
         for _ in range(self._config.rl.n_episodes): # TODO: config
             self._environment.reset()
-
             self._update_epsilon()
-            reward = jnp.zeros((n_environments,)) # Different reward per environment -- shape (n_envs,)
-
-            total_train_steps = 0
-
             environment_is_done = False
+            reward = jnp.zeros((n_environments,)) # Reward per environment -- shape (n_envs,)
+
             while not environment_is_done:
                 observations = self._environment.get_observations() # shape (n_envs, n_agents, observation_size)
-                actions = self._epsilon_greedy(observations, epsilon) # shape (n_envs, n_agents)
+                actions = self._epsilon_greedy(observations)        # shape (n_envs, n_agents)
 
                 _, reward, terminated, truncated, _ = self._environment.step(actions)
 
-                episode_reward += reward # shape (n_envs,)
                 environment_is_done = bool(jnp.logical_or(jnp.any(terminated), jnp.any(truncated)))
-
+                episode_reward += reward                                 # shape (n_envs,)
                 next_observations = self._environment.get_observations() # shape (n_envs, n_agents, observation_size)
 
                 for agent_id in range(self._config.rl.num_agents): # TODO: config
-
-                    for environment_index in range(self._config.rl.n_envs): # TODO: add "amount_environments" to config file
+                    for environment_id in range(self._config.rl.n_envs): # TODO: config
                         self._replay_buffers[agent_id].add(
-                            obs=observations[environment_index, agent_id],
-                            act=actions[environment_index, agent_id],
-                            rew=reward[environment_index],
-                            next_obs=next_observations[environment_index, agent_id],
-                            done=bool(terminated[environment_index] | truncated[environment_index])
+                            obs=observations[environment_id, agent_id],
+                            act=actions[environment_id, agent_id],
+                            rew=reward[environment_id],
+                            next_obs=next_observations[environment_id, agent_id],
+                            done=bool(terminated[environment_id] | truncated[environment_id])
                         )
 
-                    if self._replay_buffers[agent_id].get_stored_size() >= batch_size: # TODO: add "num_agents" and "batch_size" to config file
-                        mini_batch = self._replay_buffers[agent_id].sample(batch_size) # TODO: add "num_agents" and "batch_size" to config file
+                    if self._replay_buffers[agent_id].get_stored_size() >= self._config.rl.batch_size: # TODO: config
+                        self._optimize_step(agent_id)
 
-                        # Double Q-learning
-                        # max Q(s, a) = Q(s, argmax_a Q(s, a))
-                        target_q_values = self._target_q_networks[agent_id](mini_batch['next_observation']) # shape (batch_size, n_actions)
-                        q_values = self._q_networks[agent_id](mini_batch['next_observation'])               # shape (batch_size, n_actions)
-
-                        greedy_actions = jnp.argmax(q_values, axis=-1, keepdims=True) # shape (batch_size, 1)
-
-                        max_next_q_values = jnp.take_along_axis(target_q_values, greedy_actions, axis=-1)
-
-                        # Target
-                        targets = mini_batch['reward'] + self._config.rl.gamma * max_next_q_values * (1.0 - mini_batch['done']) # shape (batch_size, 1) # TODO: config
-                        targets = jax.lax.stop_gradient(targets) # Do not update parameters for inference of target # shape (batch_size, 1)
-
-                        # Calculate loss and update Q-network parameters
-                        def loss_fn(model, obs, actions, targets):
-                            q_values = model(obs) # shape (batch_size, n_actions)
-                            taken_actions = actions.astype(jnp.int32) # shape (batch_size, 1)
-
-                            action_q_values = jnp.take_along_axis(q_values, taken_actions, axis=-1) # shape (batch_size, 1)
-                            loss = jnp.mean((action_q_values - targets) ** 2) # MSE loss
-                            return loss
-                        
-                        loss, grads = nnx.value_and_grad(loss_fn)(self._q_networks[agent_id], mini_batch['observation'], mini_batch['action'], targets)
-                        self._q_network_optimizers[agent_id].update(self._q_networks[agent_id], grads)
-
-                        # Update target networks periodically
                         total_train_steps += 1
-                        if total_train_steps % self._config.rl.target_update_freq == 0: # TODO config
+                        if total_train_steps % self._config.rl.target_update_freq == 0: # TODO: config
                             self._synchronize_target_networks()
-
 
     def save_model(self, base_name: str = 'model'):
         """Saves the model parameters of all Q-networks to disk with a timestamped filename.
@@ -128,22 +90,56 @@ class IQLOptimizer:
         - A timestamp to ensure uniqueness.
         """
         for agent_id, q_network in enumerate(self._q_networks):
-            config = f'{self._config.rl.n_agents}_agents_{self._config.env.n_segments}_segments'
+            config = f'{self._config.rl.n_agents}_agents_{self._config.env.n_segments}_segments' # TODO: config
             agent = f'agent_{agent_id}'
             timestamp = time.strftime("%Y%m%d-%H%M%S")
 
             model_name = f'{base_name}_{config}_{agent}_{timestamp}'
             q_network.save_checkpoint(model_name)
 
+    def _optimize_step(self, agent_id: int):
+        """Performs a single optimization step for the specified agent using the provided mini-batch of experiences.
+
+        This method computes the loss and gradients for the Q-network of the specified agent and updates its parameters accordingly.
+
+        :param agent_id: The identifier of the agent for which to perform the optimization step.
+        """
+        mini_batch = self._replay_buffers[agent_id].sample(self._config.rl.batch_size) # TODO: config
+
+        # Double Q-learning
+        target_q_values = self._target_q_networks[agent_id](mini_batch['next_observation']) # shape (batch_size, n_actions)
+        q_values = self._q_networks[agent_id](mini_batch['next_observation'])               # shape (batch_size, n_actions)
+
+        greedy_actions = jnp.argmax(q_values, axis=-1, keepdims=True)   # shape (batch_size, 1)
+
+        max_next_q_values = jnp.take_along_axis(target_q_values, greedy_actions, axis=-1)
+
+        targets = mini_batch['reward'] + self._config.rl.gamma * max_next_q_values * (1.0 - mini_batch['done']) # shape (batch_size, 1) # TODO: config
+        targets = jax.lax.stop_gradient(targets) # Do not update parameters for inference of target             # shape (batch_size, 1)
+        
+        loss, grads = nnx.value_and_grad(self._loss)(
+            self._q_networks[agent_id], 
+            mini_batch['observation'], 
+            mini_batch['action'], 
+            targets
+        )
+        self._q_network_optimizers[agent_id].update(self._q_networks[agent_id], grads)
+
+    def _loss(self, q_network, observations, taken_actions, targets):
+        """TODO Q update
+        """
+        q_values = q_network(observations) # shape (batch_size, n_actions)
+        action_q_values = jnp.take_along_axis(q_values, taken_actions, axis=-1) # shape (batch_size, 1)
+        loss = jnp.mean((action_q_values - targets) ** 2)
+        return loss
+
     def _epsilon_greedy(
         self, 
-        observations: jnp.ndarray,
-        epsilon: float
+        observations: jnp.ndarray
     ) -> jnp.ndarray:
         """TODO
         
         :param observations: Current environment observations (n_environments, n_agents, observation_size).
-        :param epsilon: The epsilon value for epsilon-greedy action selection.
         :return: The selected actions (n_environments, n_agents).
         """
         self._rng, *subkeys = jax.random.split(self._rng, 3)
@@ -169,7 +165,7 @@ class IQLOptimizer:
         greedy_actions = jnp.argmax(q_values, axis=-1) # shape (n_environments, n_agents)
 
         epsilon_greedy = jnp.where(
-            random_probabilities < epsilon,
+            random_probabilities < self._epsilon,
             random_actions,
             greedy_actions
         ) # shape (n_environments, n_agents)
@@ -181,7 +177,7 @@ class IQLOptimizer:
         The epsilon value is decayed by multiplying it with `epsilon_decay`, but it will not go below `epsilon_min`.
         """
         self._epsilon = max(
-            self._epsilon * self._config.rl.epsilon_decay, 
+            self._epsilon * self._config.rl.epsilon_decay, # TODO: config
             self._config.rl.epsilon_min # TODO: config
         ) 
 
