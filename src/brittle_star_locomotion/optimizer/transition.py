ff727b33b8bba15be7bdf369f321771677c7b09f
brentjan <brentjan.janssens@ugent.be>
2026-04-24 21:20:36 +0200

    chore: docstrings in Transition

--- src/brittle_star_locomotion/optimizer/transition.py
+++ src/brittle_star_locomotion/optimizer/transition.py
@@ -6,9 +6,9 @@ import jax.numpy as jnp
 @dataclass
 class Transition:
     """Class that represents a transition tuple `(s, a, r, s')` from reinforcement learning."""
-    observations: jnp.ndarray
-    actions: jnp.ndarray
-    rewards: jnp.ndarray
-    next_observations: jnp.ndarray
-    terminated: jnp.ndarray
-    truncated: jnp.ndarray
+    observations: jnp.ndarray       # shape (n_environments, n_agents, observation_size)
+    actions: jnp.ndarray            # shape (n_environments, n_agents)
+    rewards: jnp.ndarray            # shape (n_environments,)
+    next_observations: jnp.ndarray  # shape (n_environments, n_agents, observation_size)
+    terminated: jnp.ndarray         # shape (n_environments,)
+    truncated: jnp.ndarray          # shape (n_environments,)
